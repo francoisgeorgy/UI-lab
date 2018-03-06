1ecdbf69c1622b3f7ef03b3c22a03b3982225a1d
François Georgy <francois.georgy@gmail.com>
2018-03-06 16:58:55 +0100

    semantic-ui with react

---
+++ semantic-ui-react/src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import {Header} from "semantic-ui-react";
+// import './App.css';
+
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <div className="App-header">
+                    <Header  as="h1">Project Mini-Mek</Header>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default App;
